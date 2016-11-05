cc19eddd42a9960d7d2cf8082ef876b3be82372d
mgrc <mgr.cieslak@gmail.com>
2016-11-05 13:21:42 +0100

    HTML structure ready

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// ==============================
+// Main.js 
+
+$(document).ready(function(){ 
+
+	console.log('DOM READY');
+});
+
+ 
+$(window).load(function() {
+ 
+  	console.log('HTML , CSS , JS , IMG READY');
+});
