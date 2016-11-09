c44a333076de16c72bf7fdb34645a54118317fa6
mgrc <mgr.cieslak@gmail.com>
2016-11-09 21:23:08 +0100

    add Css v1.3

--- js/main.js
+++ js/main.js
@@ -6,6 +6,56 @@ $(document).ready(function(){
 	console.log('DOM READY');
 });
 
+
+//==============
+//Smooth scroll===== menu
+
+	var arrow = $("#arrow_scroll")
+	console.log(arrow);
+    arrow.on("click", function(e) {
+        e.preventDefault(); // zatrzymanie domyślnej akcji przeniesienia
+        var href = $(this).attr("href");
+        // console.log(href);
+
+        // Ważne! Przewijanie dokumentu
+        $('html, body').animate({
+            scrollTop: $(href).offset().top
+            }, 2000);
+    });
+
+//==============
+// //Smooth scroll do góry strony
+
+//     var arrow_up = $("#arrow_scroll_up")
+// 	console.log(arrow_up);
+//     arrow_up.on("click", function(e) {
+//         e.preventDefault(); // zatrzymanie domyślnej akcji przeniesienia
+//         var href = $(this).attr("href");
+//         // console.log(href);
+
+//         // Ważne! Przewijanie dokumentu
+//         $('html, body').animate
+//         ({
+//             scrollTop: 0 }, 2000, function() {
+// 			arrow_up.addClass("hide"); 
+//             });
+//     });
+
+// //==============
+// //Pojawianie się kotwicy
+// 	var $win = $(window);
+//     var winH = $win.height();
+// 	$(window).scroll(function() {
+//     if ($(window).scrollTop() > winH) {
+//         //console.log(arrow_up);
+//         arrow_up.removeClass('hide')
+
+//     }
+//     else {
+//         arrow_up.addClass('hide')
+//     }
+// });
+
  
 $(window).load(function() {
  
